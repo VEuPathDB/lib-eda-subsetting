649e9206ba2ae62951f778931dffc57752e3fc8b
Ryan Doherty <rdoherty@upenn.edu>
2021-01-17 20:57:26 -0500

    Add flag to Resources.java controlling whether to use in-memory test DB vs configured (via env vars) app db

--- src/main/java/org/veupathdb/service/edass/Resources.java
+++ src/main/java/org/veupathdb/service/edass/Resources.java
@@ -2,6 +2,7 @@ package org.veupathdb.service.edass;
 
 import org.veupathdb.lib.container.jaxrs.config.Options;
 import org.veupathdb.lib.container.jaxrs.server.ContainerResources;
+import org.veupathdb.lib.container.jaxrs.utils.db.DbManager;
 import org.veupathdb.service.edass.service.Studies;
 import org.veupathdb.service.edass.stubdb.StubDb;
 
@@ -14,8 +15,13 @@ import javax.sql.DataSource;
  * should be registered.
  */
 public class Resources extends ContainerResources {
+
+  private static final boolean USE_IN_MEMORY_TEST_DATABASE = true;
+
   public Resources(Options opts) {
     super(opts);
+    if (!USE_IN_MEMORY_TEST_DATABASE)
+      DbManager.initApplicationDatabase(opts);
   }
 
   /**
@@ -31,6 +37,8 @@ public class Resources extends ContainerResources {
   }
 
   public static DataSource getApplicationDataSource() {
-    return StubDb.getDataSource();
+    return USE_IN_MEMORY_TEST_DATABASE
+      ? StubDb.getDataSource()
+      : DbManager.applicationDatabase().getDataSource();
   }
 }

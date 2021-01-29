cddce2c3acbe9a1853fdab841ab0798c08f88c9d
Ryan Doherty <rdoherty@upenn.edu>
2021-01-29 12:38:57 -0500

    configure query logger

--- src/main/java/org/veupathdb/service/edass/Main.java
+++ src/main/java/org/veupathdb/service/edass/Main.java
@@ -1,11 +1,14 @@
 package org.veupathdb.service.edass;
 
+import org.gusdb.fgputil.db.slowquery.QueryLogConfig;
+import org.gusdb.fgputil.db.slowquery.QueryLogger;
 import org.veupathdb.lib.container.jaxrs.config.Options;
 import org.veupathdb.lib.container.jaxrs.server.ContainerResources;
 import org.veupathdb.lib.container.jaxrs.server.Server;
 
 public class Main extends Server {
   public static void main(String[] args) {
+    QueryLogger.initialize(new QueryLogConfig(){});
     new Main().start(args);
   }
 

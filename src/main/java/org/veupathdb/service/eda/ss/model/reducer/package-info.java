2b48031f5d538f73b2b7ecb5a31d1fc44fc4d325
Dan Galdi <dmgaldi@upenn.edu>
2022-07-07 14:00:02 -0400

    Update javadoc on reducer package

--- src/main/java/org/veupathdb/service/eda/ss/model/reducer/package-info.java
+++ src/main/java/org/veupathdb/service/eda/ss/model/reducer/package-info.java
@@ -10,9 +10,12 @@
  * data points or output data points in our tabular result. The nodes open binary-encoded files, each file
  * corresponding to a particular variable, in order to filter and output data. These datastreams are joined by
  * the {@link org.veupathdb.service.eda.ss.model.reducer.StreamIntersectMerger} utility class which takes
- * N sorted sets of data and outputs a sorted stream representing the intersection of the sets.
+ * N sorted sets of data and outputs a sorted stream representing the intersection of the sets. Note that as data
+ * flows toward the root of the tree, an {@link org.veupathdb.service.eda.ss.model.reducer.ancestor.AncestorExpander}
+ * or {@link org.veupathdb.service.eda.ss.model.reducer.ancestor.DescendantCollapser} is used to map the ID indexes
+ * output representing the child node's entity to the ID index of the entity represented by the parent node.
  *
- * A more detailed look into the design can be found in the following documentation page:
+ * A high-level look into the design can be found in the following documentation page:
  * https://veupathdb.atlassian.net/wiki/spaces/UI/pages/18448410/EDA+Subsetting+Optimization+WIP
  */
 package org.veupathdb.service.eda.ss.model.reducer;

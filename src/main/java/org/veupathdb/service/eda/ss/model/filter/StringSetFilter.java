99067436b32359986116fefc8db06e17b41291eb
Dan Galdi <dmgaldi@upenn.edu>
2022-10-21 12:30:59 -0400

    Fix bug causing crash on certain string set filters

--- src/main/java/org/veupathdb/service/eda/ss/model/filter/StringSetFilter.java
+++ src/main/java/org/veupathdb/service/eda/ss/model/filter/StringSetFilter.java
@@ -14,12 +14,12 @@ public class StringSetFilter extends SingleValueFilter<byte[], StringVariable> {
   private static final long BIT_MASK = 0xFF;
 
   private final List<String> _stringSet;
-  private final List<byte[]> _byteArrays;
+  private final List<byte[]> _stringSetByteArrays;
   
   public StringSetFilter(String appDbSchema, Entity entity, StringVariable variable, List<String> stringSet) {
     super(appDbSchema, entity, variable);
     _stringSet = stringSet;
-    _byteArrays = stringSet.stream()
+    _stringSetByteArrays = stringSet.stream()
         .map(s -> s.getBytes(StandardCharsets.UTF_8))
         .collect(Collectors.toList());
   }
@@ -32,17 +32,23 @@ public class StringSetFilter extends SingleValueFilter<byte[], StringVariable> {
   @Override
   public Predicate<byte[]> getPredicate() {
     return candidateByteArray -> {
-      long stringLength = 0;
+      long candidateLength = 0;
       for (int i = 0; i < Integer.BYTES; i++) {
-        stringLength <<= 8; // Shift one byte, 8 bits.
-        stringLength |= (candidateByteArray[i] & BIT_MASK);
+        candidateLength <<= 8; // Shift one byte, 8 bits.
+        candidateLength |= (candidateByteArray[i] & BIT_MASK);
       }
-      for (byte[] byteArray: _byteArrays) {
-        for (int i = 0; i < stringLength; i++) {
-          if (byteArray[i] != candidateByteArray[i + 4]) {
+      for (byte[] byteArray : _stringSetByteArrays) {
+        if (candidateLength != byteArray.length) {
+          // Shortcut skip if length is not equal.
+          continue;
+        }
+        for (int i = 0; i < candidateLength; i++) {
+          // If any character is mismatched, skip this record.
+          if (byteArray[i] != candidateByteArray[i + Integer.BYTES]) {
             break;
           }
-          if (i == stringLength - 1) {
+          // Made it to the end of the string, this is a match.
+          if (i == candidateLength - 1) {
             return true;
           }
         }

00bd7924cbc23a8633be13f277c8da2a236e5f45
Dan Galdi <dmgaldi@upenn.edu>
2023-01-20 15:09:18 -0500

    Fix variable name

--- src/test/java/org/veupathdb/service/eda/ss/model/reducer/formatter/MultiValueFormatterTest.java
+++ src/test/java/org/veupathdb/service/eda/ss/model/reducer/formatter/MultiValueFormatterTest.java
@@ -18,11 +18,11 @@ public class MultiValueFormatterTest {
         new VariableValueIdPair<>(1, "1".getBytes(StandardCharsets.UTF_8)),
         new VariableValueIdPair<>(3, "3".getBytes(StandardCharsets.UTF_8))
     );
-    MultiValueFormatter v = new MultiValueFormatter();
+    MultiValueFormatter formatter = new MultiValueFormatter();
     FormattedTabularRecordStreamer.ValueStream<byte[]> valueStream =
-        new FormattedTabularRecordStreamer.ValueStream<>(utf8Strings.iterator(), v);
-    Assertions.assertEquals("[0-first,0-second]", new String(v.format(valueStream, 0), StandardCharsets.UTF_8));
-    Assertions.assertEquals("[1]", new String(v.format(valueStream, 1), StandardCharsets.UTF_8));
-    Assertions.assertEquals("[3]", new String(v.format(valueStream, 3), StandardCharsets.UTF_8));
+        new FormattedTabularRecordStreamer.ValueStream<>(utf8Strings.iterator(), formatter);
+    Assertions.assertEquals("[0-first,0-second]", new String(formatter.format(valueStream, 0), StandardCharsets.UTF_8));
+    Assertions.assertEquals("[1]", new String(formatter.format(valueStream, 1), StandardCharsets.UTF_8));
+    Assertions.assertEquals("[3]", new String(formatter.format(valueStream, 3), StandardCharsets.UTF_8));
   }
 }

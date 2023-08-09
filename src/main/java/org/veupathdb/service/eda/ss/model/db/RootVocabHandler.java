0a402a2ed581c1eccfcc8c9a2dbbf9319e7221f6
Dan Galdi <dmgaldi@upenn.edu>
2023-08-09 13:41:19 -0400

    Add filter-awareness to study vocab

--- src/main/java/org/veupathdb/service/eda/ss/model/db/RootVocabHandler.java
+++ src/main/java/org/veupathdb/service/eda/ss/model/db/RootVocabHandler.java
@@ -4,12 +4,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.gusdb.fgputil.db.runner.SQLRunner;
 import org.veupathdb.service.eda.ss.model.Entity;
+import org.veupathdb.service.eda.ss.model.filter.Filter;
 import org.veupathdb.service.eda.ss.model.tabular.TabularResponses;
 import org.veupathdb.service.eda.ss.model.variable.VariableWithValues;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.gusdb.fgputil.FormatUtil.NL;
 
 /**
  * Business logic class for retrieving vocabularies per study.
@@ -32,8 +37,12 @@ public class RootVocabHandler {
                               DataSource dataSource,
                               Entity studyEntity,
                               VariableWithValues vocabularyVariable,
-                              TabularResponses.ResultConsumer resultConsumer) {
-    new SQLRunner(dataSource, getSqlForVocabQuery(schema, vocabularyVariable.getEntity(), studyEntity, vocabularyVariable)).executeQuery(rs -> {
+                              TabularResponses.ResultConsumer resultConsumer,
+                              List<Filter> filters) {
+    List<Filter> vocabFilters = filters.stream()
+        .filter(filter -> filter.filtersOnVariable(vocabularyVariable))
+        .collect(Collectors.toList());
+    new SQLRunner(dataSource, getSqlForVocabQuery(schema, vocabularyVariable.getEntity(), studyEntity, vocabularyVariable, vocabFilters)).executeQuery(rs -> {
       try {
         resultConsumer.begin();
         while (rs.next()) {
@@ -53,12 +62,29 @@ public class RootVocabHandler {
     });
   }
 
-  private String getSqlForVocabQuery(String appDbSchema, Entity outputEntity, Entity studyEntity, VariableWithValues vocabVar) {
+  private String getSqlForVocabQuery(String appDbSchema,
+                                     Entity outputEntity,
+                                     Entity studyEntity,
+                                     VariableWithValues vocabVar,
+                                     List<Filter> filters) {
     final String attributeValTable = appDbSchema + DB.Tables.AttributeValue.NAME(outputEntity);
-    return "SELECT DISTINCT " + studyEntity.getPKColName() + ", string_value, number_value, date_value FROM " + attributeValTable + " value \n" +
+    List<String> selectColsList = new ArrayList<>(outputEntity.getAncestorPkColNames());
+    selectColsList.add(outputEntity.getPKColName());
+    String selectCols = String.join(", ", selectColsList);
+
+    // default WITH body assumes no filters. we use the ancestor table because it is small
+    String withBody = "  SELECT " + selectCols + " FROM " + appDbSchema + DB.Tables.Ancestors.NAME(outputEntity) + NL;
+
+    if (!filters.isEmpty()) {
+      withBody = filters.stream().map(Filter::getSql).collect(Collectors.joining("INTERSECT"));
+    }
+    return "" +
+        " WITH subset AS ( " + withBody + ")" + "\n" +
+        " SELECT DISTINCT ancestors." + studyEntity.getPKColName() + ", value.string_value, value.number_value, value.date_value FROM " + attributeValTable + " value \n" +
         " JOIN " + appDbSchema + DB.Tables.Ancestors.NAME(outputEntity) + " ancestors \n" +
         " ON ancestors." + outputEntity.getPKColName() + " = value." + outputEntity.getPKColName() + " \n " +
-        " WHERE attribute_stable_id = '" + vocabVar.getId() + "' \n " +
-        " ORDER BY " + studyEntity.getPKColName();
+        " JOIN subset ON subset." + outputEntity.getPKColName() + " = value." + outputEntity.getPKColName() + "\n" +
+        " WHERE value.attribute_stable_id = '" + vocabVar.getId() + "' \n " +
+        " ORDER BY ancestors." + studyEntity.getPKColName();
   }
 }

7f49476b9c483d1be25ca02509e6847c82aaae33
Ryan Doherty <tech@conical.org>
2023-11-17 12:44:27 -0500

    Add logging while trying to find files metadata and increment version for release

--- src/main/java/org/veupathdb/service/eda/ss/model/reducer/MetadataFileBinaryProvider.java
+++ src/main/java/org/veupathdb/service/eda/ss/model/reducer/MetadataFileBinaryProvider.java
@@ -1,5 +1,7 @@
 package org.veupathdb.service.eda.ss.model.reducer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.veupathdb.service.eda.ss.model.Entity;
 import org.veupathdb.service.eda.ss.model.variable.BinaryProperties;
 import org.veupathdb.service.eda.ss.model.variable.binary.BinaryFilesManager;
@@ -11,6 +13,9 @@ import java.util.Optional;
  * used by a consumer of the generated binary files.
  */
 public class MetadataFileBinaryProvider implements BinaryMetadataProvider {
+
+    private static final Logger LOG = LogManager.getLogger(MetadataFileBinaryProvider.class);
+
     private BinaryFilesManager binaryFilesManager;
 
     public MetadataFileBinaryProvider(BinaryFilesManager binaryFilesManager) {
@@ -20,12 +25,21 @@ public class MetadataFileBinaryProvider implements BinaryMetadataProvider {
     @Override
     public Optional<BinaryProperties> getBinaryProperties(String studyAbbrev, Entity entity, String variableId) {
         if (!binaryFilesManager.studyHasFiles(studyAbbrev)) {
+            LOG.error("Could not find files for study '" + studyAbbrev + "' (using entity '" + entity.getId() + "' and variable '" + variableId + "') while trying to find binary properties for a variable.");
             return Optional.empty();
         }
         Optional<BinaryFilesManager.Metadata> metadata = binaryFilesManager.readMetadata(studyAbbrev, entity);
+        if (metadata.isEmpty()) {
+            LOG.error("Unable to read metadata file for study '" + studyAbbrev + "' (using entity '" + entity.getId() + "') even though files are present.");
+            return Optional.empty();
+        }
         Optional<BinaryFilesManager.VariableMeta> varMetadata = metadata.flatMap(meta -> meta.getVariableMetadata().stream()
             .filter(varMeta -> varMeta.getVariableId().equals(variableId))
             .findFirst());
-        return varMetadata.map(varMeta -> varMeta.getProperties());
+        if (varMetadata.isEmpty()) {
+            LOG.error("Unable to find metadata object for study '" + studyAbbrev + "', entity '" + entity.getId() + "', and variable '" + variableId + "' in metadata file.");
+            return Optional.empty();
+        }
+        return Optional.of(varMetadata.get().getProperties());
     }
 }

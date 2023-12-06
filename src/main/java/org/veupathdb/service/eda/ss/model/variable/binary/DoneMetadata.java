5b2fc882da3aa9f66612f92b32970a122a0d1809
Dan Galdi <dmgaldi@upenn.edu>
2023-12-06 09:39:32 -0500

    Add done metadata to binary files

---
+++ src/main/java/org/veupathdb/service/eda/ss/model/variable/binary/DoneMetadata.java
@@ -0,0 +1,15 @@
+package org.veupathdb.service.eda.ss.model.variable.binary;
+
+public class DoneMetadata {
+  private Long dataVersion;
+
+  public DoneMetadata() {}
+
+  public Long getDataVersion() {
+    return dataVersion;
+  }
+
+  public void setDataVersion(long dataVersion) {
+    this.dataVersion = dataVersion;
+  }
+}

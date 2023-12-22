f4aed1a144890defc215f2c25397a5adb669b9a2
Dan Galdi <dmgaldi@upenn.edu>
2023-12-22 15:45:15 -0500

    Use latest FGP to fix NPE in binary files

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
 
 // Project settings
 group   = "org.veupathdb.eda"
-version = "4.13.7"
+version = "4.13.8"
 
 plugins {
   `java-library`
@@ -108,7 +108,7 @@ repositories {
 dependencies {
 
   // FgpUtil Dependencies
-  val fgputil = "2.12.15-jakarta"
+  val fgputil = "2.12.16-jakarta"
   implementation("org.gusdb:fgputil-core:${fgputil}")
   implementation("org.gusdb:fgputil-db:${fgputil}")
   implementation("org.gusdb:fgputil-json:${fgputil}")

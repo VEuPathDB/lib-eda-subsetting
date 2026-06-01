cfe9f5fa6873594e9fb172c94b35b830b7ae45c2
steve-fischer-200 <sfischer@upenn.edu>
2026-06-01 13:34:36 -0400

    add comment about empty strings

--- src/main/java/org/veupathdb/service/eda/subset/model/db/ResultSetUtils.java
+++ src/main/java/org/veupathdb/service/eda/subset/model/db/ResultSetUtils.java
@@ -43,6 +43,7 @@ public class ResultSetUtils {
 
   public static String getRsOptionalString(ResultSet rs, String columnName, String defaultVal) throws SQLException {
     String val = rs.getString(columnName);
+    // oracle treats empty string as null.  do that here for pg
     return (val == null || val.isEmpty()) ? defaultVal : val;
   }
 
@@ -80,6 +81,7 @@ public class ResultSetUtils {
 
   private static String getRsString(ResultSet rs, String columnName, boolean requireNonNull, String typeDisplay) throws SQLException {
     String value = rs.getString(columnName);
+    // oracle treats empty string as null.  do that here for pg
     if ((rs.wasNull() || value.isEmpty()) && requireNonNull) {
       String typeAnnot = typeDisplay == null ? "" : " (" + typeDisplay + ")";
       throw new RuntimeException("Column " + columnName + " returned a null or empty value but is required" + typeAnnot + ".");
